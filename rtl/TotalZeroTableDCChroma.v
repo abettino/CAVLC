e89f9b32027548aa1ed6ef0a8dc192bac560f808
abettino <abettino@b0027048-07b2-11de-b753-7319d013a8fa>
2009-04-07 16:24:54 +0000

    added dc chroma total zero table

---
+++ rtl/TotalZeroTableDCChroma.v
@@ -0,0 +1,72 @@
+module TotalZeroTableDCChroma (
+                               input [8:0] Bits,
+                               input [3:0] TotalCoeff,
+                               output reg [3:0] TotalZeroes,
+                               output reg [3:0] NumShift
+                               );
+
+
+always @* begin
+  case (TotalCoeff)
+    4'h1 : begin
+      if (Bits[8]) begin
+        TotalZeroes = 0;
+        NumShift = 1;
+      end
+      else if (Bits[8:7]=='b01) begin
+        TotalZeroes = 1;
+        NumShift = 2;
+      end
+      else if (Bits[8:6]=='b001) begin
+        TotalZeroes = 2;
+        NumShift = 3;
+      end
+      else if (Bits[8:6]=='b000) begin
+        TotalZeroes = 3;
+        NumShift = 3;
+      end
+      else begin
+        TotalZeroes = 0;
+        NumShift = 0;
+      end
+    end
+    4'h2 : begin
+      if (Bits[8]) begin
+        TotalZeroes = 0;
+        NumShift = 1;
+      end
+      else if (Bits[8:7]=='b01) begin
+        TotalZeroes = 1;
+        NumShift = 2;
+      end
+      else if (Bits[8:7]=='b00) begin
+        TotalZeroes = 2;
+        NumShift = 2;
+      end
+      else begin
+        TotalZeroes = 0;
+        NumShift = 0;
+      end
+    end
+    4'h3 : begin
+      if (Bits[8]) begin
+        TotalZeroes = 0;
+        NumShift = 1;
+      end
+      else if (Bits[8]=='b0) begin
+        TotalZeroes = 1;
+        NumShift = 1;
+      end
+      else begin
+        TotalZeroes = 0;
+        NumShift = 0;
+      end
+    end
+    default : begin
+        TotalZeroes = 0;
+        NumShift = 0;
+    end
+  endcase
+end
+
+endmodule
